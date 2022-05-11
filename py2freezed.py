d49ae8779d7dcca47c67637674fa274fba2b830c
J-P Nurmi <jp.nurmi@canonical.com>
2022-05-11 14:04:32 +0200

    Initial class support

--- py2freezed.py
+++ py2freezed.py
@@ -12,9 +12,43 @@ class Py2Freezed(ast.NodeVisitor):
     def visit_ClassDef(self, node):
         if any(base.value.id == 'enum' for base in node.bases):
             self.nodes.append(Py2FreezedEnum(node))
+        else:
+            self.nodes.append(Py2FreezedClass(node))
 
     def to_freezed(self):
-        return '\n'.join(str(node) for node in self.nodes)
+        return ''.join(str(node) for node in self.nodes)
+
+class Py2FreezedClass(ast.NodeVisitor):
+    def __init__(self, node: ast.ClassDef):
+        self.name = node.name
+        self.properties = []
+        self.visit(node)
+
+    def visit_AnnAssign(self, node):
+        self.properties.append(Py2FreezedProperty(node))
+
+    def __str__(self):
+        properties = '\n    '.join(str(property) for property in self.properties)
+        return f'''
+@freezed
+class {self.name} with _${self.name} {{
+  @JsonSerializable(explicitToJson: true, fieldRename: FieldRename.snake)
+  const factory {self.name}({{
+    {properties}
+  }}) = _{self.name};
+
+  factory {self.name}.fromJson(Map<String, dynamic> json) => _${self.name}FromJson(json);
+}}
+'''
+
+class Py2FreezedProperty(ast.NodeVisitor):
+    def __init__(self, node: ast.AnnAssign):
+        self.name = node.target.id
+        self.optional = False
+        self.visit(node)
+
+    def __str__(self):
+        return f'''// TODO: {self.name}'''
 
 class Py2FreezedEnum(ast.NodeVisitor):
     def __init__(self, node: ast.ClassDef):
